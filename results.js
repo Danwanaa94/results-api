20e7e2dc4927ba0899b23be1c13a332bade12d27
Danwanaa94 <raufyussif04@gmail.com>
2022-04-06 15:25:32 +0000

    first commit

---
+++ results.js
@@ -0,0 +1,14 @@
+const results =[
+    {name:"Wind Rauf", class:"form one", marks:8},
+    {name:"Wolo Dari", class:"form one", marks:6},
+    {name:"sung Ara", class:"form one", marks:7},
+    {name:"dan yussif", class:"form one", marks:9},
+    {name:"Nur sam", class:"form one", marks:7},
+    {name:"Nur sam", class:"form one", marks:32},
+    {name:"Nur sam", class:"form one", marks:23},
+    {name:"Nur sam", class:"form one", marks:40},
+    {name:"Nur sam", class:"form one", marks:20},
+    {name:"Nur sam", class:"form one", marks:10},
+]
+
+module.exports = results
